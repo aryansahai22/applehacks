4b03bd51f4fd8fd843491e5d8f3dae76f6080523
aryansahai22 <aryan22@stanford.edu>
2024-09-07 20:33:08 +0800

    Initial Commit

---
+++ SnapWell/SnapWellApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapWellApp.swift
+//  SnapWell
+//
+//  Created by Aryan Sahai on 07/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SnapWellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
